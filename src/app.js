1d5fefbae2852d63fe007b2f22b4427dbcc2fa5f
sanwebclick <101254127+sanwebclick@users.noreply.github.com>
2022-03-12 17:06:27 +0530

    port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.POST || 8000;
+const port = process.env.PORT || 8000;
 
 const path = require("path");
 const staticPath = path.join(__dirname, "../public");
